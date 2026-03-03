be300c8c5c930f8ab42f24f923f343ae98055f20
dpwanjala <dpwanjala@gmail.com>
2026-03-03 14:16:52 -0800

    chore: remove legacy Next.js codebase

--- pages/account/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export const PageList: React.FC = () => {
-  return <div>list profiles page</div>;
-};
-export default PageList;
--- pages/sessions/show/[id].tsx
+++
@@ -1,8 +0,0 @@
-// ShowPage.tsx
-import ShowPageComponent from "./ShowPageComponent";
-
-export const ShowPage: React.FC = () => {
-  return <ShowPageComponent />;
-};
-
-export default ShowPage;
